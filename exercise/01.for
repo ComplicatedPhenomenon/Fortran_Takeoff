57989de75d3cfda8c6be312d1f8a1f96a77c8327
ComplicatedPhenomenon <czfzdxx@outlook.com>
2017-02-18 20:45:06 +0800

    2nd  commit

---
+++ exercise/01.for
@@ -0,0 +1,50 @@
+<<<<<<< HEAD
+      block data bd
+          real :: x = 0.123
+          common / c / x
+      end block data
+
+      program common_blocks
+          interface   !?
+              function x_plus_y(y)
+                  real::x_plus_y,y
+              end function
+           end interface
+
+          real::y=0.564
+          print"(a,f5.3)","x_plus_y=",x_plus_y(y)
+     
+          stop
+      end
+ 
+      real function x_plus_y(y)
+          real::x,y !don't need to pass 'x' as argument
+          common / c / x !'x' is shared via common block 'c'
+          x_plus_y=x+y
+          return
+      end
+=======
+      real function quadf (x,a,b,c)
+      !
+      !
+      !
+      !
+      implicit none
+      real,intent(in) :: x
+      real,intent(in) :: a
+      real,intent(in) :: b
+      real,intent(in) :: c
+      quadf=a*x**2+b*x+c
+      end function quadf
+
+      program test_quadf
+      implicit none
+      real :: quadf
+      real :: a,b,c,x
+      write (*,*) 'enter quadratic coefficients a,b,and c:'
+      read (*,*) a,b,c
+      write (*,*)'enter x'
+      read (*,*) x
+      write (*,*)'quadf =',quadf(x,a,b,c)
+      end program test_quadf
+>>>>>>> 854050ac0255a92e44494d18fa0a94b5ff4087e3
---
+++ exercise/05.for
@@ -0,0 +1,20 @@
+c      module shared_data
+c      implicit none
+c      save
+c
+c      real :: pi
+c      pi=3.141592653d0
+c      end module shared_data
+
+      program volume_of_ball
+      use shared_data
+      implicit none
+
+      real :: v
+      real :: r
+      print *,*'r'
+      read *,*,r
+      v=4/3*pi*r**3
+      print *,*'V is ',v
+      end program volume_of_ball
+
