3b30b7a99257b6d45090f4e4c2e5abf708abe461
ComplicatedPhenomenon <czfzdxx@outlook.com>
2017-04-05 22:34:17 +0800

    little progress

--- exercise/array/2Darray.for
+++ exercise/array/2Darray.for
@@ -1,20 +1,22 @@
-      program 2Darray
+      program oneDarray
       implicit none
 
       real :: Total=0.0,Average=0.0
-      real , dimension(1:12):: rainfall 
+      real , dimension(1:12) :: rainfall 
       integer :: month
-      print *,'Type in yje rainfall values'
+
+      print *,'Type in month rainfall values'
       print *,'one per line'
-      do month=1,12
-        read *,rainfall(month)
-      enddo
-      do month=1,12
+      do month=1, 12
+        read *, rainfall(month)
         total=total+rainfall(month)
       enddo
       average=total/12
       print *,'Average monthly rainfall was'
       print *,Average
-      
-      end program 2Darray
+      do month = 1, 12
+      print *,rainfall(month)
+      end do 
+
+      end program oneDarray
 
