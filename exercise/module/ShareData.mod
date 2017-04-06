dc91db0e9605df06fe7c93e0e93e079247ef78b6
ComplicatedPhenomenon <czfzdxx@outlook.com>
2017-04-06 11:59:16 +0800

    Whole morning gone

--- exercise/module/ShareData.mod
+++
@@ -1,8 +0,0 @@
-      module shared_data 
-      implicit none
-      save
-
-      real :: pi
-      pi=3.141592653d0
-      end module shared_data
-
---
+++ exercise/module/test_1.for
@@ -0,0 +1,10 @@
+      program test1
+
+      use shared_data
+      implicit none
+
+      print *,'pi is      ', pi
+      print *,'sigmaz is  ', sigmaz
+      print *,'sigmay is  ', sigmay
+       
+      end program test1
