6896f5af077ce16ae5f3a7bd98f3f50139699876
ComplicatedPhenomenon <czfzdxx@outlook.com>
2017-04-07 19:15:51 +0800

    least number of var

--- exercise/files/l_interval_1.f
+++ exercise/files/l_interval_1.f
@@ -1,8 +1,18 @@
       program l_interval_1
       implicit none
 
-      real, dimension (1:3) : x, y
-      real :: i,l
+      real, dimension (1:3) :: x, y
+      integer :: i
+      real :: temp, l
       print *,'Input the first vector'
       read (*,*) x(1),x(2),x(3)
       print *,'Input the second vector'
+      read (*,*) y(1),y(2),y(3)
+      temp = 0
+      do i = 1, 3
+      temp = (x(i)-y(i)) ** 2 + temp
+      end do
+      l = sqrt (temp)
+      print *,l
+
+      end program l_interval_1
