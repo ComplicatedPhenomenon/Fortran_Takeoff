f13486c903c865230e130ae741ac7d07bb9d24f2
ComplicatedPhenomenon <czfzdxx@outlook.com>
2017-04-07 19:04:14 +0800

    0

---
+++ exercise/files/l_interval_1.f
@@ -0,0 +1,8 @@
+      program l_interval_1
+      implicit none
+
+      real, dimension (1:3) : x, y
+      real :: i,l
+      print *,'Input the first vector'
+      read (*,*) x(1),x(2),x(3)
+      print *,'Input the second vector'
