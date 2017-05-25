01b1b1813cf046edc499a8354ff4904abb28de9e
ComplicatedPhenomenon <czfzdxx@outlook.com>
2017-05-26 07:45:38 +0800

    ><

--- exercise/comp_old_Fortran/main.f90
+++ exercise/comp_old_Fortran/main.f90
@@ -1,32 +1,36 @@
+module bb
+   public :: fxn_f, fxn_g 
+   contains 
+   function fxn_f(x) result(f)
+   real(kind(0d0)) :: x, f     
+   f = x ** 2
+   end function fxn_f 
+
+   function fxn_g(x) result(g)
+   real(kind(0d0)) :: x, g    
+   g = x ** 3
+   end function fxn_g
+end module bb
 program main
-use MC_VEGAS 
-implicit none
+   use bb
+   use MC_VEGAS 
+   implicit none
 
-real(kind(0d0)), external :: f, g
-integer                   :: NDIM
-real(kind(0d0))           :: avgi_f, avgi_g, sd, chi2a
-
-NDIM=1
-open(1,file = 'DATA.txt', status='unknown')
-call vegas(NDIM,f,avgi_f,sd,chi2a)
-call vegas(NDIM,g,avgi_g,sd,chi2a)
-write(1,*)'TEST',avgi_f,avgi_g
-close(1)
-print *, 'avgi_f = ', avgi_f
-print *, 'avgi_g = ', avgi_g
+   real(kind(0d0))           :: f, g
+   real(kind(0d0))           :: x
+   integer                   :: NDIM
+   real(kind(0d0))           :: avgi_f, avgi_g, sd, chi2a
+   NDIM=1
+   open(1,file = 'DATA.txt', status='unknown')
+   call vegas(NDIM,fxn_f,avgi_f,sd,chi2a)
+   call vegas(NDIM,fxn_g,avgi_g,sd,chi2a)
+   write(1,*)'TEST'
+   write(1,*)'avfi_f= ',avgi_f
+   write(1,*)'avfi_g= ',avgi_g
+   close(1)
+   print *, 'avgi_f = ', avgi_f
+   print *, 'avgi_g = ', avgi_g
 end program main
-real(kind=8) function f(x)
-implicit none
-double precision x
-      
-f = x ** 2
-end function 
-
-real(kind=8) function g(x)
-implicit none
-
-double precision  x
-g = x ** 3
-end function 
-
 include "MC_VEGAS.f90"
+
+
