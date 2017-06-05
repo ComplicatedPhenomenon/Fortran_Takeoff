78590a4d80c7c54e6ad36fdc34ca2eae7d550287
ComplicatedPhenomenon <czfzdxx@outlook.com>
2017-06-05 22:00:03 +0800

    >>>

--- exercise/comp_old_Fortran/FXN.f90
+++ exercise/comp_old_Fortran/FXN.f90
@@ -9,12 +9,10 @@
 module my_fxn
    implicit none   
    private
-   public :: fxn_1, fxn_2             ! for external use
-                                      ! PRIVATE :: subname_1, subname_2
-                                      ! all entities listed in PRIVATE will NOT be accessible from outside of the module 
-   ! (Declared shared data here)      ! and all entities listed in PUBLIC can be accessed fron the out side of the module
+   public :: fxn_1, fxn_2           
+                                     
    
-!  real(kind(0d0)), parameter      :: A=64000000d0
+   real(kind(0d0)), parameter      :: A=64000000d0
    real(kind(0d0)), parameter      :: m=172d0
    real(kind(0d0)), parameter      :: Q=2d0 
    real(kind(0d0)), parameter      :: pi=3.14159d0
@@ -53,23 +51,20 @@ module my_fxn
 !------------------------------------------!
 !  momentum variable 
 !------------------------------------------!
-      subroutine commonpart(A, t, u, f, tau_0)
-!     subroutine commonpart(t, u, f, tau_0)
+      subroutine commonpart(x, t, u, f, tau_0)
          implicit none
-         real(kind(0d0)) :: A
          real(kind(0d0)) :: t, u, f, tau_0
-         real(kind(0d0)) :: theta, cos_theta, sin_theta
+         real(kind(0d0)) :: x, sin_theta
          real(kind(0d0)), dimension(0:3) :: p_1, p_2, p_3, p_4
-!        print *, A
-!        pause
-         cos_theta = cos(theta)
-         sin_theta = sqrt(1-cos_theta**2) 
+
+         sin_theta = sqrt(1-x**2) 
+
          p_1 = [sqrt(s)/2d0,0d0,0d0, sqrt(s)/2d0]
          p_2 = [sqrt(s)/2d0,0d0,0d0, sqrt(s)/2d0]
-         p_3 = [sqrt(s)/2d0, 0d0, sqrt(s/4-m**2)*sin_theta, sqrt(s/4-m**2)*cos_theta]
-         p_4 = [sqrt(s)/2d0, 0d0, -sqrt(s/4-m**2)*sin_theta, -sqrt(s/4-m**2)*cos_theta]
+         p_3 = [sqrt(s)/2d0, 0d0, sqrt(s/4-m**2)*sin_theta, sqrt(s/4-m**2)*x]
+         p_4 = [sqrt(s)/2d0, 0d0, -sqrt(s/4-m**2)*sin_theta, -sqrt(s/4-m**2)*x]
 
-         t = (m**2-2*dot_vec(p_1,p_3))  ! using the name of function and its input to call the output of function is okay
+         t = (m**2-2*dot_vec(p_1,p_3))  
          u = (m**2-2*dot_vec(p_1,p_4))
          f = (m**2-t)*(m**2-u) 
          tau_0 = m**2/A
@@ -80,30 +75,25 @@ module my_fxn
 
       function fxn_1( x, wgt) result(fxn_qq)       ! NO `,` 
          implicit none
-         real(kind(0d0)), dimension(2:3) :: x
+         real(kind(0d0)), dimension(1:3) :: x
          real(kind(0d0)) :: wgt
-         real(kind(0d0)) :: A
          real(kind(0d0)) :: t, u, f, tau_0
          real(kind(0d0)) :: part_qq, fxn_qq 
-         real(kind(0d0))  :: theta
          real(kind(0d0)), dimension(1:3) :: upper, lower
          real(kind(0d0)) :: jfactor
-         A = 64000000d0
 
          wgt = 0
          s=x(2)*x(3)*A
          sqrts=sqrt(s)
          m_res=2*m
-!        print *, A
-!        pause
          if (sqrts < m_res)then
             fxn_qq=0d0
             return
           else 
          endif
-!        call commonpart(t, u, f, tau_0)      ! It also works when A is a global variable, or A won't get a correct value  
+         
+         call commonpart(x(1), t, u, f, tau_0)   
 
-         call commonpart(A, t, u, f, tau_0)   ! Pass A to another procedure
          part_qq = 0d0
          do i = 1, 5
             part_qq = part_qq+CT14Pdf(i, x(2), Q)*CT14Pdf(-i, x(3), Q) * &
@@ -113,10 +103,8 @@ module my_fxn
 
          upper = [1d0, 1d0, 1d0]
          lower = [-1d0, tau_0, tau_0/x(2)]
-         theta = (upper(1) - lower(1))*theta+lower(1)
-         x(2) = (upper(2) - lower(2))*x(2)+lower(2)
-         x(3) = (upper(3) - lower(3))*x(3)+lower(3)
-         jfactor = jacobian(upper, lower)          ! It's another way but with more var defined
+         x = (upper - lower)*x+lower
+         jfactor = jacobian(upper, lower)       
          fxn_qq = jfactor * part_qq
 
       end function fxn_1
@@ -126,16 +114,13 @@ module my_fxn
 
       function fxn_2( x, wgt) result(fxn_gg)
          implicit none 
-         real(kind(0d0)), dimension(2:3) :: x
+         real(kind(0d0)), dimension(1:3) :: x
          real(kind(0d0)) :: wgt
-         real(kind(0d0)) :: A
          real(kind(0d0)) :: t, u, f, tau_0
-         real(kind(0d0)) :: part_gg,fxn_gg                          ! notice that you don't define the intent of the result
-         real(kind(0d0)) :: theta
+         real(kind(0d0)) :: part_gg,fxn_gg   
          real(kind(0d0)), dimension(1:3) :: upper, lower
          real(kind(0d0)) :: jfactor
 
-         A = 64000000d0
          wgt = 0
          s = x(2) * x(3) * A
          sqrts = sqrt(s)
@@ -145,8 +130,8 @@ module my_fxn
             return
          else 
          endif
-         call commonpart(A, t, u, f, tau_0)
-!         call commonpart(t, u, f, tau_0)
+
+         call commonpart(x(1), t, u, f, tau_0)
          part_gg = CT14Pdf(0,x(2),Q)*CT14Pdf(0,x(3),Q)* &
                   2*pi*(a_s**2/(32*s))*sqrt(1d0-4*m**2/s)*  &                                            
                   (6/s**2*f-(m**2*(s-4*m**2))/(3*f) +       &
@@ -157,9 +142,7 @@ module my_fxn
 
          upper = [1d0, 1d0, 1d0]
          lower = [-1d0, tau_0, tau_0/x(2)]
-         theta = (upper(1) - lower(1))*theta+lower(1)
-         x(2) = (upper(2) - lower(2))*x(2)+lower(2)
-         x(3) = (upper(3) - lower(3))*x(3)+lower(3)
+         x = (upper - lower)*x+lower
          jfactor = jacobian(upper, lower)
          fxn_gg = jfactor * part_gg
       end function fxn_2
--- exercise/cs/fxn.f90
+++ exercise/cs/fxn.f90
@@ -3,14 +3,12 @@ module my_fxn
    private
    public :: fxn_2   
    
-   
-   real(kind(0d0)), parameter      :: S=64000000d0
    real(kind(0d0)), parameter      :: m=172d0
    real(kind(0d0)), parameter      :: Q=2d0 
    real(kind(0d0)), parameter      :: pi=3.14159d0
    real(kind(0d0)), parameter      :: a_s = 0.12d0
    real(kind(0d0)), external       :: CT14pdf
-   real(kind(0d0)) :: sqrts,m_res
+   real(kind(0d0)) :: s, sqrts,m_res
    integer         :: i
    contains        
       function jacobian( upper, lower) result(jfactor)
@@ -40,20 +38,22 @@ module my_fxn
       
       
 
-      subroutine commonpart(p3_0, p4_0,cos_theta,eta, gm, s12, k_v, p4_v, s13, s14, s23, s24) 
+      subroutine commonpart(p4_v,k_v,s13, s14, s23, s24, tau_0)
          implicit none
-         real(kind(0d0)) :: s12,  s13, s14, s23, s24
+        ! real(kind(0d0)) :: S
+         real(kind(0d0)) :: s12,  s13, s14, s23, s24, tau_0 ! for output
          real(kind(0d0)) :: p3_v, p4_v, k_v   ! intermediate vars
-         real(kind(0d0)) :: cos_theta, sin_theta, &
+         real(kind(0d0)) :: theta, cos_theta, sin_theta, &
                             eta, cos_eta, sin_eta,        &
                             ksi, cos_ksi, sin_ksi,        &
                             p3_0, p4_0, gm
          real(kind(0d0)), dimension(0:3) :: k1, k2, p3, p4, k ! 5 kinematics vars
 
-         p3_v = sqrt(p3_0**2-m**2)    !Do you see you make p3_0 satisfy 0<p3_0<1
+         p3_v = sqrt(p3_0**2-m**2) 
          p4_v = sqrt(p4_0**2-m**2) 
          k_v = sqrt((sqrt(s12)-p3_0-p4_0)**2-gm**2)
 
+         cos_theta = cos(theta)
          sin_theta = sqrt(1-cos_theta**2) 
          cos_eta = cos(eta)
          sin_eta = sqrt(1-cos_eta**2)  ! ?wm?
@@ -67,28 +67,24 @@ module my_fxn
          p4 = [p4_0, p4_v*sin_theta, 0d0, p4_v*cos_theta]
          k  = 0 - p3 - p4
 
-         print *,'k1=',k1
-         print *,'k2=',k2
-         print *, 'k=',k
-         pause
-
          s13 = (m**2- 2*dot_vec(k1,p3))
          s14 = (m**2- 2*dot_vec(k1,p4))
          s23 = (m**2- 2*dot_vec(k2,p3))
          s24 = (m**2- 2*dot_vec(k2,p3))
+         tau_0 = m**2/S
 
       end subroutine commonpart
 
 
 
 
-      function fxn_2(z, wgt) result(fxn_gg)
+      function fxn_2( x, wgt) result(fxn_gg)
          implicit none 
+         real(kind(0d0)), dimension(1:2) :: x               ! 2 var
          real(kind(0d0)), dimension(1:7) :: z               ! used to store the 7 variables of integration
-         real(kind(0d0)), dimension(1:7) :: tem               ! used to store the 7 variables of integration
-         real(kind(0d0)), dimension(1:2) :: x               ! used to store the 7 variables of integration
          real(kind(0d0)) :: wgt
-         real(kind(0d0)) :: m_res, tau_0
+         !
+         real(kind(0d0)) :: tau_0
          real(kind(0d0)) :: p3_0, p4_0, theta, eta, gm      ! other 5 vars
          real(kind(0d0)) :: sigma, tau, m_plus, m_minus,  &   ! intermediate var 
                             p3_v, p4_v, k_v
@@ -98,18 +94,17 @@ module my_fxn
                             p3_0_min, p4_0_min, theta_min, eta_min, gm_min, x1_min, x2_min
          real(kind(0d0)), dimension(1:7) :: upper, lower
          real(kind(0d0)) :: jfactor
-
          sunn = 3                                          !!! only a test
+         S = 64000000d0
          wgt = 0
-         tau_0 = m**2/S
-         print *, z
-         pause
-!-----------------------------------------------------------
-!        z = [ p3_0, p4_0, theta, eta, gm, x(1),x(2)]
-!-----------------------------------------------------------
+         s12 = x(1) * x(2) * S
+
+         z = [ p3_0, p4_0, theta, eta, gm, x(1),x(2)]
+         call commonpart(p4_v,k_v,s13, s14, s23, s24, tau_0) 
+       
 
          sigma = sqrt(s12)-p4_0
-         tau = sigma**2 - p4_v**2
+         tau = sigma**2 - k_v**2
          m_plus = m + gm
          m_minus = m - gm
          
@@ -121,8 +116,8 @@ module my_fxn
          theta_min = -1
          eta_max = 2*pi
          eta_min = 0
-         gm_max = sqrt(s12)/4                                      !!! 
-         gm_min = sqrt(s12)/10
+         gm_max = s12                                      !!! 
+         gm_min = 0
          x1_max = 1
          x1_min = 1/tau_0
          x2_max = 1
@@ -131,14 +126,10 @@ module my_fxn
          upper = [p3_0_max, p4_0_max, theta_max, eta_max, gm_max, x1_max, x2_max]
          lower = [p3_0_min, p4_0_min, theta_min, eta_min, gm_min, x1_min, x2_min]
 
-         s12 = z(6)*z(7) * S
          z = (upper - lower)*z + lower  ! feel the power of using advanced concept
-
-         call commonpart(z(1), z(2), z(3),z(4), z(5), s12, k_v, p4_v, s13, s14, s23, s24) 
          
          jfactor = jacobian(upper, lower)
          
-         print *, 'jfactor', jfactor
 
          m_res = 2*m
          if (sqrt(s12) < m_res)then
