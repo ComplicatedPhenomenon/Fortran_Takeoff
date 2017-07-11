4edb0951e584f2af70ede1dbaae09c97f9a61ffb
ComplicatedPhenomenon <czfzdxx@outlook.com>
2017-07-11 16:38:32 +0800

    Not just do it

---
+++ exercise/modern_fotran/subr_func.f90
@@ -0,0 +1,5 @@
+!**************************************************************
+! Any function can be converted to a subroutine by moving the *
+! result variable tp a dummy argument with `intent(out)`, the *
+! opposite process may be more problematic                    *
+!**************************************************************
